92aa2bf3b4704cae96e7a2ec23c5875a721ef670
TOSHIBA <TOSHIBA@DESKTOP-CEDDLTI>
2025-03-22 00:28:05 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace For_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("Countdown");
+
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(1 + i);
+            }
+            Console.WriteLine("Blast offf!");
+
+            Console.WriteLine();
+
+            Console.WriteLine("x         y");
+
+            for (int i = -10; i <= 10; i = i + 2)
+            {
+                Console.Write(i);
+                Console.WriteLine("\t" + (i * i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter your name:");
+            string name = Convert.ToString(Console.ReadLine());
+
+            if (name.ToLower() == "aldworth")
+            {
+                for (int i = 0; i <= 4; i = i + 1)
+                {
+                    Console.WriteLine(i + 1 + "." + name);
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= 9; i = i + 1)
+                {
+                    Console.WriteLine(i + 1 + "." + name);
+                }
+            }
+        }
+    }
+}
